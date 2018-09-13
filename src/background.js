c53215deeda4f5b5879cf7bb4daf90aeea161793
Daniel Katz <daniel@mmcubit.com>
2018-09-13 17:00:15 +0300

    Add localization

--- src/background.js
+++ src/background.js
@@ -13,27 +13,27 @@
   chrome.runtime.onInstalled.addListener(function () {
 
     var id = chrome.contextMenus.create({
-      "title": "Force Direction",
+      "title": chrome.i18n.getMessage("i10n_rootMenu"),
       "contexts": ["editable"],
       "id": "root"
     });
 
     chrome.contextMenus.create({
-      "title": "Left to Right",
+      "title": chrome.i18n.getMessage("i10n_ltrCommandMenu"),
       "contexts": ["editable"],
       "parentId": id,
       "id": "ltr"
     });
 
     chrome.contextMenus.create({
-      "title": "Right to Left",
+      "title": chrome.i18n.getMessage("i10n_rtlCommandMenu"),
       "contexts": ["editable"],
       "parentId": id,
       "id": "rtl"
     });
 
     chrome.contextMenus.create({
-      "title": "Reset",
+      "title": chrome.i18n.getMessage("i10n_resetCommandMenu"),
       "contexts": ["editable"],
       "parentId": id,
       "id": "reset"
