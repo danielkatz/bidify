e0d803e1296209bc6db53e4eb331c41c7b550017
Daniel Katz <daniekatz@gmail.com>
2018-09-20 22:52:57 +0300

    Configure junit output

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,12 @@ module.exports = {
   testEnvironment: 'node',
   reporters: ["default",
     ["jest-junit", {
-      output: "./test-results/jest/results.xml"
+      output: "./test-results/jest/results.xml",
+      suiteName: "jest tests",
+      classNameTemplate: "{classname}-{title}",
+      titleTemplate: "{classname}-{title}",
+      ancestorSeparator: " › ",
+      usePathForSuiteName: "true"
     }]
   ]
 };
